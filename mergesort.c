caec736ea22cbcdb97b451e09fa83d91ac799572
mattcurran00 <mattcurran1212@gmail.com>
2024-12-06 13:49:13 +0000

    Add files via upload
    A merge sort algorithm that I implemented for a factory.

---
+++ mergesort.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function to merge two subarrays of arr[]
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    // Create temporary arrays
+    int L[n1], R[n2];
+
+    // Copy data to temporary arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temporary arrays back into arr[l..r]
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[], if any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[], if any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// Recursive function to sort an array using Merge Sort algorithm
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        // Find the middle point
+        int m = l + (r - l) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        // Merge the sorted halves
+        merge(arr, l, m, r);
+    }
+}
+
+int main() {
+    int arr[40];
+    int i;
+
+    // Generating 40 random integers
+    printf("Randomly generated integers:\n");
+    for (i = 0; i < 40; i++) {
+        arr[i] = rand() % 100; // Random numbers between 0 and 99
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // Applying Merge Sort
+    mergeSort(arr, 0, 39);
+
+    // Printing the sorted array
+    printf("\nSorted array:\n");
+    for (i = 0; i < 40; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
+    return 0;
+}
